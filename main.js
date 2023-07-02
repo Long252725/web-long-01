e22e4a1bc536222f8039234dfaae5ea5401bea9f
long252725 <inthanhlong2006@gmail.com>
2023-07-02 20:00:07 +0700

    testttt

---
+++ main.js
@@ -0,0 +1,4 @@
+function getClick(){
+    document.querySelector('#slide1').style.display = "none"
+    document.querySelector('#slide2').style.display = "block"
+    document.querySelector('#slide2').style.backgroundSize = '10%'}
